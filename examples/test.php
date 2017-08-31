b1e973011ca82b80a82141530c638648a3486080
Matt Rink <matt.rink@gmail.com>
2017-08-31 22:01:24 +0100

    Commented the example.

--- examples/test.php
+++ examples/test.php
@@ -4,10 +4,12 @@ require "../vendor/autoload.php";
 
 use MattRink\TinyCrashReporter\TinyCrashReporter;
 
+// Set a exception handler outside of TinyCrashReporter
 set_exception_handler(function(\Throwable $e) {
     echo "Moose: {$e->getMessage()}";
 });
 
+// Set a error handler outside of TinyCrashReporter
 set_error_handler(function(int $errNo , string $errStr, string $errFile, int $errLine, array $errContext) {
 
     echo "Moose: {$errFile}:{$errLine} {$errStr}<br/>";
@@ -16,10 +18,13 @@ set_error_handler(function(int $errNo , string $errStr, string $errFile, int $er
 
 });
 
+// Create an instance
 $reporter = new TinyCrashReporter();
 
+// Throw an exception
 throw new Exception("This is a test");
 
+// Or a notice 
 $i = 1/0;
 
 echo "This is a test.";
