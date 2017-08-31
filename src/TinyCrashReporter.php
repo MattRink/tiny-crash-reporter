f4d0c60ed82917aa66dee28fd52115bf06e480d2
Matt Rink <matt.rink@gmail.com>
2017-08-31 19:58:24 +0100

    Initial version of working reporter.

--- src/TinyCrashReporter.php
+++ src/TinyCrashReporter.php
@@ -7,7 +7,37 @@ class TinyCrashReporter
     
     public function __construct()
     {
-        
+
+        $this->setExceptionHandler();
+
+        $this->setErrorHandler();
+
+    }
+
+    protected function setExceptionHandler()
+    {
+
+        set_exception_handler(function(\Throwable $e) {
+
+            $message = $e->getMessage();
+
+            echo $message;
+
+        });
+
+    }
+
+    protected function setErrorHandler()
+    {
+
+        set_error_handler(function(int $errno , string $errstr, string $errfile, int $errline, array $errcontext) {
+
+            $message = $errstr;
+
+            echo $message;
+
+        });
+
     }
 
 }
